7ebabaa6e0b5f72f4bb55ac75170898760f05dc6
Omprakash Jadhav <97115491+omprakashjadhav87@users.noreply.github.com>
2026-04-18 21:41:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+
+region = "us-east-1"
+}
+
+resource "aws_instance" "myec2-cloud" {
+
+ami = "ami-0ec10929233384c7f"
+instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance1"
+  }
+
+
+
+}
