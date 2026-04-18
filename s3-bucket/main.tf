bfc7f02612dfeed70b26be729650ed171d4c0937
Omprakash Jadhav <97115491+omprakashjadhav87@users.noreply.github.com>
2026-04-18 22:26:21 +0530

    Update main.tf

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -28,13 +28,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 }
 }
 }
-
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.terraform-state.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
