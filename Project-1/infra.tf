a99634c72ef8433d530d59ff6925307b0ecf5947
Omprakash Jadhav <97115491+omprakashjadhav87@users.noreply.github.com>
2026-04-20 09:00:18 +0530

    Add security group ports variable and ingress rules
    Added a variable for security group ports and created a dynamic ingress rule in the AWS security group resource.

--- Project-1/infra.tf
+++ Project-1/infra.tf
@@ -59,3 +59,39 @@ gateway_id = aws_internet_gateway.gw.id
 
 }
 
+variable "sg_ports" {
+type = list(number)
+default = [8080,80,22,443]
+
+}
+
+resource "aws_security_group" "sl-sg" {
+  name        = "sg_rule"
+  vpc_id = aws_vpc.sl-vpc.id
+  dynamic  "ingress" {
+    for_each = var.sg_ports
+    iterator = port
+    content{
+    from_port        = port.value
+    to_port          = port.value
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+  }
+egress {
+
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+
+}
+
+}
+
+resource "tls_private_key" "mykey" {
+  algorithm = "RSA"
+
+}
+
